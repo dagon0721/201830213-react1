a94907602827f5dc3ba27f189fc977df6c317bbd
dagon0721 <b5959946c@gmail.com>
2024-04-03 10:41:03 +0900

    Update

--- src/App.js
+++ src/App.js
@@ -22,4 +22,12 @@ function App() {
   );
 }
 
+function Welcome(props){
+  return <h1>안녕, {props.name}</h1>;
+}
+const element = <Welcome name = "인제" />;
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
 export default App;
